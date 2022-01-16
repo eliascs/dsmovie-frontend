cd782e3ea10e25c2b5570f23b987361872ca8c0e
Elias Cardoso <eliascardosodasilva@hotmail.com>
2022-01-16 15:15:13 -0300

    first commit

--- src/pages/Listing/index.tsx
+++ src/pages/Listing/index.tsx
@@ -1,7 +1,33 @@
+import MovieCard from "../../components/MovieCard";
+import Pagination from "C:/projetos/dsmovie/frontend/src/components/Pagination";
+
 function Listing() {
 
-    return(
-        <h1>Página de listagem</h1>
+    return (
+        <>
+            <Pagination />
+
+            <div className="contaier">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="mb-3 col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+            
+                </div>
+            </div>
+        </>
     );
 }
 
